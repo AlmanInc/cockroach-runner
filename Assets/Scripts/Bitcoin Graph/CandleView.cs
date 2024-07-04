e12afd23ee04c773cd6369a5addbf93cdd4e941b
Moskalenko Andrey <sevhwarang@gmail.com>
2024-07-04 17:39:02 +0300

    Implemented drawing for candle shadows

--- Assets/Scripts/Bitcoin Graph/CandleView.cs
+++ Assets/Scripts/Bitcoin Graph/CandleView.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 namespace CockroachRunner
@@ -7,9 +8,9 @@ namespace CockroachRunner
     {
         [Space]
         [SerializeField] private RectTransform topBar;
-        [SerializeField] private RectTransform topShadow;
+        [SerializeField] private Image topShadow;
         [SerializeField] private RectTransform bottomBar;
-        [SerializeField] private RectTransform bottomShadow;
+        [SerializeField] private Image bottomShadow;
 
         [Space]
         [SerializeField] private Color upColor;
@@ -18,7 +19,7 @@ namespace CockroachRunner
         [Inject] private GraphView graphView;
 
         private bool isActive;
-        
+
         public float StartPrice { get; set; }
         public float EndPrice { get; set; }
         public float MinPrice { get; set; }
@@ -29,9 +30,9 @@ namespace CockroachRunner
             isActive = false;
 
             topBar.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0f);
-            topShadow.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0f);
+            topShadow.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0f);
             bottomBar.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0f);
-            bottomShadow.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0f);
+            bottomShadow.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0f);
         }
 
         public void DrawCandle(float price)
@@ -65,7 +66,38 @@ namespace CockroachRunner
                 }
 
                 bar.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+
+                if (EndPrice < MinPrice)
+                {
+                    MinPrice = EndPrice;
+                }
+
+                if (EndPrice > MaxPrice)
+                {
+                    MaxPrice = EndPrice;
+                }
+
+                topShadow.color = EndPrice >= StartPrice ? upColor : downColor;
+                bottomShadow.color = EndPrice >= StartPrice ? upColor : downColor;
+
+                if (EndPrice > MinPrice && MinPrice < StartPrice)
+                {
+                    DrawShadow(bottomShadow.rectTransform, MinPrice);
+                }
+
+                if (EndPrice < MaxPrice && MaxPrice > StartPrice)
+                {
+                    DrawShadow(topShadow.rectTransform, MaxPrice);
+                }
             }
+        }
+
+        private void DrawShadow(RectTransform shadowBar, float price)
+        {
+            float Y = graphView.GetPriceYPosition(price);
+            float shadowHeight = Mathf.Abs(transform.position.y - Y);
+
+            shadowBar.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, shadowHeight);
         }
     }
 }
