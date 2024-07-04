597503bf08a572864b19dd4a33ab161c9acc262e
Moskalenko Andrey <sevhwarang@gmail.com>
2024-07-04 12:11:32 +0300

    Implemented math function for graph (find actual position on graphic for any price)

--- Assets/Scripts/Bitcoin Graph/GraphView.cs
+++ Assets/Scripts/Bitcoin Graph/GraphView.cs
@@ -4,12 +4,20 @@ namespace CockroachRunner
 {
     public class GraphView : MonoBehaviour
     {
-        [SerializeField] private float minPrice;
-        [SerializeField] private float maxPrice;
+        [SerializeField] private float startMinPrice;
+        [SerializeField] private float startMaxPrice;
         [SerializeField] private PriceLine[] lines;
+
+        [SerializeField] private RectTransform start;
+        [SerializeField] private float startPrice;
+
+        private float minPrice;
+        private float maxPrice;
 
         private void Start()
         {
+            minPrice = startMinPrice;
+            maxPrice = startMaxPrice;
             SetNewMinMaxPrices(minPrice, maxPrice);
         }
 
@@ -22,6 +30,20 @@ namespace CockroachRunner
                 float price = max - i * priceStep;
                 lines[i].LabelPrice.text = string.Format("{0:0.000}", price);
             }
+        }
+
+        private float GetPriceYPosition(float price)
+        {                                    
+            float deltaPriceProgress = Mathf.Clamp01((price - minPrice) / (maxPrice - minPrice));
+            print(deltaPriceProgress);
+            return Mathf.Lerp(lines[lines.Length - 1].transform.position.y, lines[0].transform.position.y, deltaPriceProgress);
+        }
+
+        public void Update() 
+        {
+            Vector3 position = start.position;
+            position.y = GetPriceYPosition(startPrice);
+            start.position = position;
         }
     }
 }
